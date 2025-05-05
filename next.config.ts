dcb7d91112e0b59b29cb50c656b6d5b5a72e8223
Stanley <kestanley101@gmail.com>
2025-05-05 10:19:43 -0400

    fix: remove api limiter from next config as it is deprecated and added prisma generate script to package.json

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  api: {
-    bodyParser: {
-      sizeLimit: "10kb",
-    },
-  },
+
   images: {
     domains: ["ui-avatars.com"],
   },
